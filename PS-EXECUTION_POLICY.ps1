7dbf619e8418968b44e953d1209ff2b8741ff93a
GuiMunizR <guilherme.muniz@outlook.com.br>
2022-06-04 00:54:12 -0300

    Commit 20220604-1253AM

---
+++ PS-EXECUTION_POLICY.ps1
@@ -0,0 +1,13 @@
+﻿#Gets all execution policy values for the session listed in precedence order.
+Get-ExecutionPolicy -List
+
+#CurrentUser, LocalMachine, MachinePolicy, Process or UserPolicy
+$Scope = "CurrentUser"
+
+#AllSigned, Bypass, Default, RemoteSigned, Restricted, Undefined or Unrestricted.
+$ExecutionPolicy = "AllSigned"
+
+#Sets the PowerShell execution policies for Windows computers.    
+Set-ExecutionPolicy -Scope $Scope -ExecutionPolicy $ExecutionPolicy 
+
+#Font: https://docs.microsoft.com/pt-pt/powershell/module/microsoft.powershell.security/set-executionpolicy?view=powershell-7.2
